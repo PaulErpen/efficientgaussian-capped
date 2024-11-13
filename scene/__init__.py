2235a5cd93d496ce3d838a3768785b2d317ad9ef
Paul Erpenstein <paul.erpenstein@gmail.com>
2024-11-13 15:57:58 +0100

    Add pointcloud initialization

--- scene/__init__.py
+++ scene/__init__.py
@@ -49,7 +49,7 @@ class Scene:
         self.test_cameras = {}
 
         if os.path.exists(os.path.join(args.source_path, "sparse")):
-            scene_info = sceneLoadTypeCallbacks["Colmap"](args.source_path, args.images, eval=True)
+            scene_info = sceneLoadTypeCallbacks["Colmap"](args.source_path, args.images, eval=True, n_start_gaussians=args.n_start_gaussians)
         elif os.path.exists(os.path.join(args.source_path, "transforms_train.json")):
             print("Found transforms_train.json file, assuming Blender data set!")
             scene_info = sceneLoadTypeCallbacks["Blender"](args.source_path, args.white_background, eval=True, n_start_gaussians=args.n_start_gaussians)
